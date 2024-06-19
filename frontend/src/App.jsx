90c1aba2892e9c179c80c71ffbbd8fcf541ef0c9
kaz <kazkianoush03@gmail.com>
2024-06-19 15:12:15 -0700

    routing fix 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 import "./App.css";
 
-import AuthPage from "./AuthPage";
-import ChatsPage from "./ChatsPage";
+import AuthPage from "./AuthPage.jsx";
+import ChatsPage from "./ChatsPage.jsx";
 
 function App() {
   const [user, setUser] = useState(undefined);
